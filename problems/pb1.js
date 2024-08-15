b1741073d8c64c7de61820b39128cdeda19f362d
O'Malley <edward.omalley@fiserv.com>
2024-08-15 15:55:35 -0500

    first

--- problems/pb1.js
+++ problems/pb1.js
@@ -1,5 +1,10 @@
 // declare and define function here
 
+    function replaceSpaces(str, char) {
+    stri = str.replaceAll(' ', char);
+    console.log(stri);
+    return stri;
+}
 
 // test cases
 // replaceSpaces('Today is a good day', '-'); // -> 'Today-is-a-good-day'
--- problems/pb2.js
+++ problems/pb2.js
@@ -1,6 +1,31 @@
 // declare and define function here
 
+function mostFrequentChar(str) {
+    characters = []
+    for (i = 0; i < str.length; i++) {
+        char = str.charAt(i).toUpperCase()
+        if(i == 0){
+            console.log("first",char)
+            characters.push({char: char, num:0})
+        }
+        for (j = 0; j < characters.length; j++) {
+            temp = characters[j]
+            if (temp.char == char) {
+                characters[j].num++;
+                continue
+            }
+            else {
 
+                if(j == characters.length-1){
+                    console.log(temp.char,char)
+                 characters.push({ char: char, num: 0 })
+                }
+            }
+            
+        }
+
+    }
+}
 // test cases
 // mostFrequentChar('bookeeper'); // -> 'e'
 // mostFrequentChar('mississippi'); // -> 'i'. 'i' and 's' appear with same frequency in 'mississippi' but 'i' appears first!
